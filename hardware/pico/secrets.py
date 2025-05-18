b9df3d9325c1cae0b314bba26c7382773172e9b1
James Matthews <james30152@gmail.com>
2025-05-18 16:11:08 -0400

    Delete Pico, updated secrets

--- hardware/pico/secrets.py
+++
@@ -1,7 +0,0 @@
-# 🔒 COPY this file to secrets.py and add your real Wi-Fi credentials.
-# DO NOT COMMIT secrets.py to GitHub.
-
-secrets = {
-    'ssid': 'jamesgang',
-    'password': 'I4cuabru1!'
-}
